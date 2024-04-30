3f3c7942a660849482a9bfdb86c8bb80a57e8ea1
Jose Espejo <jespagu2610@g.educaand.es>
2024-04-30 13:02:27 +0200

    Commit PedidoProducto

--- src/main/java/com/example/api/model/PedidoProducto.java
+++ src/main/java/com/example/api/model/PedidoProducto.java
@@ -1,28 +1,27 @@
 package com.example.api.model;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "`pedido?producto`")
+@Table(name = "`pedido_producto`")
 public class PedidoProducto {
 
     @Id
     @Column(name = "id", nullable = false)
-    private PedidoProductoId id;
+    private Integer id;
 
-    @MapsId("idProd")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_prod", nullable = false)
-    private Producto idProd;
+    private Producto producto;
 
-    @MapsId("idPed")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_ped", nullable = false)
-    private Pedido idPed;
+    private Pedido pedido;
 
     @Column(name = "cantidad", nullable = false)
     private Integer cantidad;
